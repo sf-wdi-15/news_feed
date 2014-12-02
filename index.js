9d3d548ea736cb9c56cb3e46ce566f50587001b1
Michael Desa <mjdesa@gmail.com>
2014-12-02 11:53:12 -0800

    Add basic scaffold for index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var body-parser = require('body-parser');
+
+var app = express();
+
+var articles = [];
+
+app.get('/articles', function(req,res) {
+
+});
+
+app.get('/articles/new', function(req,res) {
+
+});
+
+app.post('/articles', function(req,res) {
+
+});
