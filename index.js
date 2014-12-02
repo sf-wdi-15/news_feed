a9ec562e84dbf2f7b7d52138091fc9afe90fe678
Michael Desa <mjdesa@gmail.com>
2014-12-02 12:15:35 -0800

    Build basic rendering of pages

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var express = require('express');
-var body-parser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.set('view engine', 'ejs');
 
 var articles = [];
 
 app.get('/articles', function(req,res) {
-
+  res.render('articles/index');
 });
 
 app.get('/articles/new', function(req,res) {
-
+  res.render('articles/new');
 });
 
 app.post('/articles', function(req,res) {
@@ -18,13 +20,17 @@ app.post('/articles', function(req,res) {
 });
 
 app.get('/', function(req,res) {
-
+  res.render('site/index.ejs');
 });
 
 app.get('/about', function(req,res) {
-
+  res.render('site/about');
 });
 
 app.get('/contact', function(req,res) {
+  res.render('site/contact');
+});
 
+app.listen(3000, function() {
+  console.log('Listening');
 });
